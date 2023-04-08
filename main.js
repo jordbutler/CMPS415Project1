d01c7631205aba9f651d501db1e67094fecfb29b
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-07 21:31:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-//this line is required to parse the request body
 app.use(express.json())
-/* Create - POST method */
+
+/* POST Method */
 app.post('/user/add', (req, res) => {
-    //get the existing user data
+    
     const existUsers = getUserData()
     
-    //get the new user data from post request
     const userData = req.body
-    //check if the userData fields are missing
+    
     if (userData.fullname == null || userData.age == null || userData.username == null || userData.password == null) {
         return res.status(401).send({error: true, msg: 'User data missing'})
     }
@@ -20,25 +19,28 @@ app.post('/user/add', (req, res) => {
     if (findExist) {
         return res.status(409).send({error: true, msg: 'username already exist'})
     }
-    //append the user data
+    
     existUsers.push(userData)
-    //save the new user data
+    
     saveUserData(existUsers);
     res.send({success: true, msg: 'User data added successfully'})
 })
-/* Read - GET method */
+
+/* GET-All Method */
 app.get('/user/list', (req, res) => {
     const users = getUserData()
     res.send(users)
 })
-/* Read Single User - Get method */
+
+/* GET Method */
 app.get('/user/:username', (req, res) => {
-    //get the username from url
+    
     const username = req.params.username
-    //get the update data
+    
     const userData = req.body
-    //get the existing user data
+    
     const existUsers = getUserData()
+    
     //check if the username exist or not       
     const findExist = existUsers.find( user => user.username === username )
     if (!findExist) {
@@ -47,8 +49,8 @@ app.get('/user/:username', (req, res) => {
     return res.send(findExist)
 })
 
-/* util functions */
-//read the user data from json file
+
+
 const saveUserData = (data) => {
     const stringifyData = JSON.stringify(data)
     fs.writeFileSync('users.json', stringifyData)

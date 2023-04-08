5db9468cb190deed32c99bf3a6d5a262d23bc0c8
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-04-07 21:06:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -31,8 +31,8 @@ app.get('/user/list', (req, res) => {
     const users = getUserData()
     res.send(users)
 })
-/* Update - Patch method */
-app.patch('/user/update/:username', (req, res) => {
+/* Read Single User - Get method */
+app.get('/user/:username', (req, res) => {
     //get the username from url
     const username = req.params.username
     //get the update data
@@ -44,29 +44,9 @@ app.patch('/user/update/:username', (req, res) => {
     if (!findExist) {
         return res.status(409).send({error: true, msg: 'username not exist'})
     }
-    //filter the userdata
-    const updateUser = existUsers.filter( user => user.username !== username )
-    //push the updated data
-    updateUser.push(userData)
-    //finally save it
-    saveUserData(updateUser)
-    res.send({success: true, msg: 'User data updated successfully'})
+    return res.send(findExist)
 })
-/* Delete - Delete method */
-app.delete('/user/delete/:username', (req, res) => {
-    const username = req.params.username
-    //get the existing userdata
-    const existUsers = getUserData()
-    //filter the userdata to remove it
-    const filterUser = existUsers.filter( user => user.username !== username )
-    if ( existUsers.length === filterUser.length ) {
-        return res.status(409).send({error: true, msg: 'username does not exist'})
-    }
-    //save the filtered data
-    saveUserData(filterUser)
-    res.send({success: true, msg: 'User removed successfully'})
-    
-})
+
 /* util functions */
 //read the user data from json file
 const saveUserData = (data) => {
